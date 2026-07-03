85e9c4552f0b5e2b544952332ce598546a0ce7ba
jdmccork <50844752+jdmccork@users.noreply.github.com>
2026-07-03 19:35:06 +1200

    Fix gourmand name

--- scripts/layout_manager/layout_manager.lua
+++ scripts/layout_manager/layout_manager.lua
@@ -8,7 +8,7 @@ function CampaignChange()
         [0] = "layouts/components/campaigns/%smonk.jsonc",
         [1] = "layouts/components/campaigns/%ssurvivor.jsonc",
         [2] = "layouts/components/campaigns/%shunter.jsonc",
-        [3] = "layouts/components/campaigns/gormand.jsonc",
+        [3] = "layouts/components/campaigns/gourmand.jsonc",
         [4] = "layouts/components/campaigns/artificer.jsonc",
         [5] = "layouts/components/campaigns/rivulet.jsonc",
         [6] = "layouts/components/campaigns/spearmaster.jsonc",
