7f6c1cce6f8f88ab26838b25e7be48f22d04eb11
jdmccork <50844752+jdmccork@users.noreply.github.com>
2026-09-06 12:08:15 +1200

    Add qiuck fix for submerged logic with no perk

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -12,13 +12,21 @@ CUR_INDEX = -1
 LOCAL_ITEMS = {}
 GLOBAL_ITEMS = {}
 
+aplogicdebug = true
+
+function apprint(...)
+    if aplogicdebug then
+        print(...)
+    end
+end
+
 -- resets an item to its initial state
 function resetItem(item_code, item_type)
 	local obj = Tracker:FindObjectForCode(item_code)
 	if obj then
 		item_type = item_type or obj.Type
 		if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("resetItem: resetting item %s of type %s", item_code, item_type))
+			apprint(string.format("resetItem: resetting item %s of type %s", item_code, item_type))
 		end
 		if item_type == "toggle" or item_type == "toggle_badged" then
 			obj.Active = false
@@ -30,16 +38,16 @@ function resetItem(item_code, item_type)
 		elseif item_type == "custom" then
 			-- your code for your custom lua items goes here
 		elseif item_type == "static" and AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("resetItem: tried to reset static item %s", item_code))
+			apprint(string.format("resetItem: tried to reset static item %s", item_code))
 		elseif item_type == "composite_toggle" and AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format(
+			apprint(string.format(
 				"resetItem: tried to reset composite_toggle item %s but composite_toggle cannot be accessed via lua." ..
 				"Please use the respective left/right toggle item codes instead.", item_code))
 		elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("resetItem: unknown item type %s for code %s", item_type, item_code))
+			apprint(string.format("resetItem: unknown item type %s for code %s", item_type, item_code))
 		end
 	elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-		print(string.format("resetItem: could not find item object for code %s", item_code))
+		apprint(string.format("resetItem: could not find item object for code %s", item_code))
 	end
 end
 
@@ -49,7 +57,7 @@ function incrementItem(item_code, item_type, multiplier)
 	if obj then
 		item_type = item_type or obj.Type
 		if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("incrementItem: code: %s, type %s", item_code, item_type))
+			apprint(string.format("incrementItem: code: %s, type %s", item_code, item_type))
 		end
 		if item_type == "toggle" or item_type == "toggle_badged" then
 			obj.Active = true
@@ -64,16 +72,16 @@ function incrementItem(item_code, item_type, multiplier)
 		elseif item_type == "custom" then
 			-- your code for your custom lua items goes here
 		elseif item_type == "static" and AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("incrementItem: tried to increment static item %s", item_code))
+			apprint(string.format("incrementItem: tried to increment static item %s", item_code))
 		elseif item_type == "composite_toggle" and AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format(
+			apprint(string.format(
 				"incrementItem: tried to increment composite_toggle item %s but composite_toggle cannot be access via lua." ..
 				"Please use the respective left/right toggle item codes instead.", item_code))
 		elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("incrementItem: unknown item type %s for code %s", item_type, item_code))
+			apprint(string.format("incrementItem: unknown item type %s for code %s", item_type, item_code))
 		end
 	elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-		print(string.format("incrementItem: could not find object for code %s", item_code))
+		apprint(string.format("incrementItem: could not find object for code %s", item_code))
 	end
 end
 
@@ -120,8 +128,8 @@ function apply_slot_data(slot_data)
 	local spawn = SPAWN_TABLE[string.upper(slot_data["starting_room"])]
 	local name = SPAWN_NAMING[spawn]
 	if spawn then
-		print(string.format("%s is the starting region",spawn))
-		print(string.format("%s is the full name of starting region", name))
+		apprint(string.format("%s is the starting region",spawn))
+		apprint(string.format("%s is the full name of starting region", name))
 		Tracker:FindObjectForCode(spawn).Active = true
 		if CURRENT_CAMPAIGN == 7 and SAINT_TABLE[name] then
 			Tracker:UiHint("ActivateTab", SAINT_TABLE[name])
@@ -131,7 +139,7 @@ function apply_slot_data(slot_data)
 			Tracker:UiHint("ActivateTab", name)
 		end
 	else
-		print("Default spawn")
+		apprint("Default spawn")
 		if CURRENT_CAMPAIGN == 0 or CURRENT_CAMPAIGN == 1 then
 			Tracker:FindObjectForCode("Outskirts").Active = true
 			Tracker:UiHint("ActivateTab","Outskirts")
@@ -166,11 +174,17 @@ function apply_slot_data(slot_data)
 	outlawchecks = slot_data["difficulty_outlaw"]
 	echochecks = slot_data["difficulty_echo_low_karma"]
 
+	local perks = {}
+	for _, perk in ipairs(slot_data["expedition_perks"]) do
+		perks[perk] = true
+	end
+
 	if slot_data["difficulty_extreme_threats"] == 1 then
 		Tracker:FindObjectForCode("extreme_threats").Active = true
 	end
     
-	if slot_data["checks_submerged"] == 1 then
+	-- Needs to be changed to have config in settings for perks
+	if slot_data["checks_submerged"] == 1 and (perks["Aquatic Perk"] or CAMPAIGN_NAMING[CURRENT_CAMPAIGN] == "Rivulet") then
 		Tracker:FindObjectForCode("sub_aquatic").Active = true
 	elseif slot_data["checks_submerged"] == 2 then
 		Tracker:FindObjectForCode("sub_all").Active = true
@@ -191,7 +205,7 @@ function onClear(slot_data)
 	slot_name = string.format("RW_%s_room", Archipelago:GetPlayerAlias(Archipelago.PlayerNumber))
 	Tracker.BulkUpdate = true	
 	if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-		print(string.format("called onClear, slot_data:\n%s", dump_table(slot_data)))
+		apprint(string.format("called onClear, slot_data:\n%s", dump_table(slot_data)))
 	end
 	CUR_INDEX = -1
 	-- reset locations
@@ -199,14 +213,14 @@ function onClear(slot_data)
 		for _, location_table in ipairs(mapping_entry) do
 			for _, location_code in ipairs(mapping_entry) do
 				if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-					print(string.format("onClear: clearing location %s", location_code))
+					apprint(string.format("onClear: clearing location %s", location_code))
 				end
 				if location_code:sub(1, 1) == "@" then
 					local obj = Tracker:FindObjectForCode(location_code)
 					if obj then
 						obj.AvailableChestCount = obj.ChestCount
 					elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-						print(string.format("onClear: could not find location object for code %s", location_code))
+						apprint(string.format("onClear: could not find location object for code %s", location_code))
 					end
 				else
 					-- reset hosted item
@@ -222,9 +236,9 @@ function onClear(slot_data)
 		local item_type = item_table[2]
 		if item_code then
 			resetItem(item_code, item_type)
-			print(string.format("onClear: clearing item %s", item_code))
+			apprint(string.format("onClear: clearing item %s", item_code))
 		elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("onClear: skipping item_table with no item_code: %s",item_table))
+			apprint(string.format("onClear: skipping item_table with no item_code: %s",item_table))
 		end
 	end
 	for _, dummy_table in pairs(DUMMYITEMS) do
@@ -232,9 +246,9 @@ function onClear(slot_data)
 		local dummy_type = dummy_table[2]
 		if dummy_code then
 			resetItem(dummy_code,dummy_type)
-			print(string.format("onClear: clearing dummy item %s", dummy_code))
+			apprint(string.format("onClear: clearing dummy item %s", dummy_code))
 		elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("onClear: skipping dummy_table with no dummy_code: %s", dummy_table))
+			apprint(string.format("onClear: skipping dummy_table with no dummy_code: %s", dummy_table))
 		end
 	end
 	apply_slot_data(slot_data)
@@ -250,7 +264,7 @@ end
 -- called when an item gets collected
 function onItem(index, item_id, item_name, player_number)
 	if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-		print(string.format("called onItem: %s, %s, %s, from slot %s, %s", index, item_id, item_name, player_number, CUR_INDEX))
+		apprint(string.format("called onItem: %s, %s, %s, from slot %s, %s", index, item_id, item_name, player_number, CUR_INDEX))
 	end
 	if not AUTOTRACKER_ENABLE_ITEM_TRACKING then
 		return
@@ -263,7 +277,7 @@ function onItem(index, item_id, item_name, player_number)
 	local item_table = ITEM_MAPPING[item_name]
 	if not item_table then
 		if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("onItem: could not find item mapping for name %s", item_name))
+			apprint(string.format("onItem: could not find item mapping for name %s", item_name))
 		end
 		return
 	else
@@ -271,7 +285,7 @@ function onItem(index, item_id, item_name, player_number)
 		local item_type = item_table[2]
 		local multiplier = item_table[3] or 1
 		if item_code == "unknown" then
-			print(string.format("onItem: Unknown item %s requires mapping", item_name))
+			apprint(string.format("onItem: Unknown item %s requires mapping", item_name))
 		elseif item_code then
 			incrementItem(item_code, item_type, multiplier)
 			-- keep track which items we touch are local and which are global
@@ -289,12 +303,12 @@ function onItem(index, item_id, item_name, player_number)
 				end
 			end
 		elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("onItem: skipping empty item_table"))
+			apprint(string.format("onItem: skipping empty item_table"))
 		end
 	end
 	if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-		print(string.format("local items: %s", dump_table(LOCAL_ITEMS)))
-		print(string.format("global items: %s", dump_table(GLOBAL_ITEMS)))
+		apprint(string.format("local items: %s", dump_table(LOCAL_ITEMS)))
+		apprint(string.format("global items: %s", dump_table(GLOBAL_ITEMS)))
 	end
 	-- track local items via snes interface
 	if PopVersion < "0.20.1" or AutoTracker:GetConnectionState("SNES") == 3 then
@@ -320,7 +334,7 @@ end
 -- called when a location gets cleared
 function onLocation(location_id, location_name)
 	if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-		print(string.format("called onLocation: %s, %s", location_id, location_name))
+		apprint(string.format("called onLocation: %s, %s", location_id, location_name))
 	end
 	if not AUTOTRACKER_ENABLE_LOCATION_TRACKING then
 		return
@@ -328,7 +342,7 @@ function onLocation(location_id, location_name)
 	local mapping_entry = LOCATION_MAPPING[location_name]
 	if not mapping_entry then
 		if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-			print(string.format("onLocation: could not find location mapping for name %s", location_name))
+			apprint(string.format("onLocation: could not find location mapping for name %s", location_name))
 		end
 		return
 	end
@@ -344,7 +358,7 @@ function onLocation(location_id, location_name)
 					incrementItem(location_code, item_type)
 				end
 			elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-				print(string.format("onLocation: could not find object for code %s", location_code))
+				apprint(string.format("onLocation: could not find object for code %s", location_code))
 			end
 		end
 	end
@@ -353,7 +367,7 @@ end
 -- called when a locations is scouted
 function onScout(location_id, location_name, item_id, item_name, item_player)
 	if AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP then
-		print(string.format("called onScout: %s, %s, %s, %s, %s", location_id, location_name, item_id, item_name,
+		apprint(string.format("called onScout: %s, %s, %s, %s, %s", location_id, location_name, item_id, item_name,
 			item_player))
 	end
 	-- not implemented yet :(
@@ -361,11 +375,11 @@ end
 
 -- called when a bounce message is received
 function onBounce(json)
-	print(string.format("called onBounce: %s", dump_table(json)))
+	apprint(string.format("called onBounce: %s", dump_table(json)))
 	local roomid = nil
 	local player_number = Archipelago.PlayerNumber
 	if json.data == nil then
-		print("Invalid bounce for AutoTabbing")
+		apprint("Invalid bounce for AutoTabbing")
 		return
 	end
 	for i, slot_number in next, json.slots do
@@ -377,17 +391,17 @@ function onBounce(json)
 	end
 	
 	if not Tracker:FindObjectForCode("autotab").Active or roomid == nil then
-		print("Bounce for a different slot")
+		apprint("Bounce for a different slot")
 		return
 	end
 
 	if string.match(roomid,"GATE_") then
-		print("Bounce sent from a gate")
+		apprint("Bounce sent from a gate")
 		return
 	end
 
 	CURRENT_REGION = string.lower(string.match(roomid,"(.*)_"))
-	print(string.format(CURRENT_REGION))
+	apprint(string.format(CURRENT_REGION))
 	if CAMPAIGN_NAMING[CURRENT_CAMPAIGN] == "Saint" and SAINT_TABLE[TABS_MAPPING[CURRENT_REGION]] ~= nil then
 		roomid = string.format(SAINT_TABLE[TABS_MAPPING[CURRENT_REGION]])
 	elseif CAMPAIGN_NAMING[CURRENT_CAMPAIGN] == "Inv" and INV_TABLE[TABS_MAPPING[CURRENT_REGION]] ~= nil then
